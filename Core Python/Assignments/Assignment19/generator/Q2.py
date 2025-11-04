1cd98920ca69538ca7bedcd52629ffc52d94da86
Saurav Bhamare <145749119+Sauravbhamare@users.noreply.github.com>
2025-11-04 12:29:49 +0530

    Update Q2.py

--- Core Python/Assignments/Assignment19/generator/Q2.py
+++ Core Python/Assignments/Assignment19/generator/Q2.py
@@ -1,16 +1,3 @@
-# def palindrome(limit):
-#     n = 0
-#     while(n <= limit):
-#         if(str(n) == str(n)[::-1]):
-#             yield n
-#         n += 1
-#     return n
-# limit = int(input("Enter limit:"))        
-# gen = palindrome(limit)
-
-# for num in gen:
-#     print(num)
-
 def palindrome(limit):
     n =0
     while(n<=limit):
